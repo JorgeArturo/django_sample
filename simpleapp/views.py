4d396f4125e523c5776d82be3094f80119d385f8
JorgeArturo <ing.jorge.mecatronico@gmail.com>
2024-01-14 19:53:22 -0600

    init django

---
+++ simpleapp/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+#request ->response
+#request handler
+#action
+
+def hello(request):
+    #pull data from db
+    #Transform data
+    #send email and so on
+    return HttpResponse('HelloWorld')
